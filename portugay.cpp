e6d1159f3c538a2f5bbf36415bdd4d0225fc7f83
Leonardo / LuKi <leo.monteiro06@live.com>
2023-04-22 15:05:53 -0300

    Adicionando opção '-n' e mais algumas pequenas melhorias

--- portugay.cpp
+++ portugay.cpp
@@ -8,23 +8,25 @@
 #include <unordered_map>
 
 void printHelp() {
-    std::cout << "Uso: portugay [-h/--help] [-o NOME_ARQUIVO] ARQUIVO_ENTRADA" << std::endl;
-    std::cout << "Um programa inspirado no Portugol para fins educacionais." << std::endl;
-    std::cout << "O programa recebe o texto, traduz para C em um novo arquivo e por fim compila-o usando o GCC." << std::endl << std::endl;
-    std::cout << "Opções:" << std::endl;
-    std::cout << "  -h, --help       Exibe esta ajuda." << std::endl;
-    std::cout << "  -o NOME_ARQUIVO  Especifica o nome do arquivo de saída." << std::endl;
+    std::cout << "Uso: portugay [-h/--help] [-o NOME_ARQUIVO] ARQUIVO_ENTRADA\n";
+    std::cout << "Um programa inspirado no Portugol para fins educacionais.\n";
+    std::cout << "O programa recebe o texto, traduz para C em um novo arquivo e por fim compila-o usando o GCC.\n\n";
+    std::cout << "Opções:\n";
+    std::cout << "  -h, --help       Exibe esta ajuda.\n";
+    std::cout << "  -o NOME_ARQUIVO  Especifica o nome do arquivo de saída.\n";
+    std::cout << "  -n               Faz com que o arquivo gerado não seja compilado.\n";
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "Erro: Nenhum arquivo de entrada fornecido." << std::endl << std::endl;
+        std::cout << "Erro: Nenhum arquivo de entrada fornecido.\n\n";
         printHelp();
         return 1;
     }
 
     std::string inputFileName;
     std::string outputFileName = "out.c";
+    bool compileOnly = false;
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
@@ -36,30 +38,32 @@ int main(int argc, char* argv[]) {
                 outputFileName = argv[i + 1];
                 ++i;
             } else {
-                std::cout << "Erro: Nome do arquivo de saída não fornecido." << std::endl << std::endl;
+                std::cout << "Erro: Nome do arquivo de saída não fornecido.\n\n";
                 printHelp();
                 return 1;
             }
+        } else if (arg == "-n") {
+            compileOnly = true;
         } else {
             inputFileName = arg;
         }
     }
 
     if (inputFileName.empty()) {
-        std::cout << "Erro: Nenhum arquivo de entrada fornecido." << std::endl << std::endl;
+        std::cout << "Erro: Nenhum arquivo de entrada fornecido.\n\n";
         printHelp();
         return 1;
     }
 
     std::ifstream inputFile(inputFileName);
     if (!inputFile) {
-        std::cout << "Erro: Não foi possível abrir o arquivo de entrada." << std::endl;
+        std::cout << "Erro: Não foi possível abrir o arquivo de entrada.\n";
         return 1;
     }
 
     std::ofstream outputFile(outputFileName);
     if (!outputFile) {
-        std::cout << "Erro: Não foi possível abrir o arquivo de saída." << std::endl;
+        std::cout << "Erro: Não foi possível abrir o arquivo de saída.\n";
         return 1;
     }
 
@@ -88,6 +92,9 @@ int main(int argc, char* argv[]) {
 
     std::string line;
     while (std::getline(inputFile, line)) {
+        if (line.find("//") == 0 || line.find("/*") == 0) {
+            continue;
+        }
         for (auto const& [key, value] : keywords) {
             size_t pos = 0;
             while ((pos = line.find(key, pos)) != std::string::npos) {
@@ -95,18 +102,22 @@ int main(int argc, char* argv[]) {
                 pos += value.length();
             }
         }
-        outputFile << line << std::endl;
+        outputFile << line << "\n";
     }
 
     inputFile.close();
     outputFile.close();
 
-    std::string compilerCommand = "gcc -x c -O2 -pipe " + outputFileName;
-    int result = std::system(compilerCommand.c_str());
+    if (!compileOnly) {
+        const std::string compilerCommand = "gcc -x c -O2 -pipe " + outputFileName;
+        const int result = std::system(compilerCommand.c_str());
 
-    if(result == 0) {
-        return 0;
-    } else {
-        return 1;
+        if(result == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
+
+    return 0;
 }
